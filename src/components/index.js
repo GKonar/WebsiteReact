ff01ad00a935e046a692e17d40edf01003cc5a20
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2020-02-10 15:44:22 +0100

    Create general exports index.js/bump

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+// General exports
+export { default as LinkElement } from './LinkElement';
+export { default as Navbar } from './Navbar';
+export { default as StyleGuide } from './StyleGuide';
+export { default as Portfolio } from './Portfolio';
+export { default as CenteringContainer } from './CenteringContainer';
